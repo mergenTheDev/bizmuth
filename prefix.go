1b101d95d9d42a1765d7fc8798a26fe3b63db77e
Berkay <berkayyvz@outlook.com>
2025-02-07 01:39:57 +0300

    update

---
+++ prefix.go
@@ -0,0 +1,6 @@
+package bizmuth
+
+var (
+	prefixErr = "\033[101m\033[97m ERROR! \033[0m\033[97m"
+	reset     = "\033[0m"
+)
--- window.go
+++ window.go
@@ -1,5 +1,26 @@
 package bizmuth
 
-func createWindow() {
+import (
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
 
+func createWindow(width int, height int, title string, resizable int) *glfw.Window {
+	glfw.WindowHint(glfw.Resizable, resizable)
+
+	window, err := glfw.CreateWindow(width, height, title, nil, nil)
+
+	if err != nil {
+		panic(prefixErr + "Can't create window!" + reset)
+	}
+
+	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		panic(prefixErr + "Can't initialize OpenGL!" + reset)
+	}
+
+	gl.Viewport(0, 0, int32(width), int32(height))
+
+	return window
 }
