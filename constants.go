7663044396cb9a492cda8ade7e520749b753ffc8
Berkay <berkayyvz@outlook.com>
2025-02-07 19:52:00 +0300

    update

---
+++ constants.go
@@ -0,0 +1,12 @@
+package bizmuth
+
+import (
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+var (
+	FALSE     = glfw.False
+	TRUE      = glfw.True
+	PrefixErr = "\033[101m ERROR! \033[0m "
+	Reset     = "\033[0m"
+)
--- prefix.go
+++
@@ -1,6 +0,0 @@
-package bizmuth
-
-var (
-	PrefixErr = "\033[101m ERROR! \033[0m "
-	Reset     = "\033[0m"
-)
