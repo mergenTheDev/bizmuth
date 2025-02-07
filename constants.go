ce3772170c5c56c68fd76bf5e74f6a0e32a91c31
Berkay <berkayyvz@outlook.com>
2025-02-07 21:39:39 +0300

    Update constants.go

--- constants.go
+++ constants.go
@@ -1,12 +1,12 @@
 package bizmuth
 
-import (
-	"github.com/go-gl/glfw/v3.3/glfw"
-)
+import "github.com/go-gl/glfw/v3.3/glfw"
 
 var (
-	FALSE     = glfw.False
-	TRUE      = glfw.True
-	PrefixErr = "\033[101m ERROR! \033[0m "
-	Reset     = "\033[0m"
+	FALSE      = glfw.False
+	TRUE       = glfw.True
+	PrefixErr  = "\033[101m ERROR! \033[0m "
+	PrefixWarn = "\033[43m WARNING! \033[0m "
+	PrefixInfo = "\033[42m INFO \033[0m "
+	Reset      = "\033[0m"
 )
