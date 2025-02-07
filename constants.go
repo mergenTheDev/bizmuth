cce49cab1546dc0dc73628e4bee34bd0bc867f12
Berkay <berkayyvz@outlook.com>
2025-02-07 21:44:45 +0300

    Update constants.go

--- constants.go
+++ constants.go
@@ -6,7 +6,7 @@ var (
 	FALSE      = glfw.False
 	TRUE       = glfw.True
 	PrefixErr  = "\033[101m ERROR! \033[0m "
-	PrefixWarn = "\033[43m WARNING! \033[0m "
-	PrefixInfo = "\033[42m INFO \033[0m "
+	PrefixWarn = "\033[103m WARNING! \033[0m "
+	PrefixInfo = "\033[1022m INFO \033[0m "
 	Reset      = "\033[0m"
 )
