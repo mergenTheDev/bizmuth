c9f2ac064451d9b6c8f39eec107cc263f01d4481
Berkay <berkayyvz@outlook.com>
2025-02-14 23:38:49 +0300

    update

--- v0.0.0/camera.go
+++ v0.0.0/camera.go
@@ -1,5 +1,22 @@
 package bizmuth
 
+import "github.com/go-gl/mathgl/mgl32"
+
+var defVertexShader = `
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main() {
+	gl_Position = projection * view * model * vec4(aPos, 1.0);
+}
+` + "\x00"
+
 func CreateCamera(pos Vector2) {
-	//projection :=
+	projection := mgl32.Ortho2D(0, 800, 0, 800)
+	//view :=
 }
