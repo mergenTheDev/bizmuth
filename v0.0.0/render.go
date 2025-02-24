9ba1110170e2d79d1d65aa80ef81f613f39c2415
Berkay <mergenthedev@proton.me>
2025-02-24 22:21:45 +0300

    Update render.go

--- v0.0.0/render.go
+++ v0.0.0/render.go
@@ -2,23 +2,10 @@ package bizmuth
 
 import "github.com/go-gl/gl/v3.3-core/gl"
 
-/*var defVertexShader = `
-#version 330 core
-
-layout (location = 0) in vec3 aPos;
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
-void main() {
-	gl_Position = projection * view * model * vec4(aPos, 1.0);
-}
-` + "\x00"*/
-
 func BackgroundColor(r float32, g float32, b float32, alpha float32) {
 	gl.ClearColor(r, g, b, alpha)
 }
 
-func Add() {
-
+func Draw() {
+	//TO-DO
 }
