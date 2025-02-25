a21e95607088fe8d6270fd3967d641d3d23b5efe
Berkay <mergenthedev@proton.me>
2025-02-25 21:41:09 +0300

    Update camera.go

--- v0.0.0/camera.go
+++ v0.0.0/camera.go
@@ -1,18 +1,20 @@
 package bizmuth
 
-import "github.com/go-gl/gl/v3.3-core/gl"
+import (
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/mathgl/mgl32"
+)
 
 var defVertexShader = `
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
 
-uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-	gl_Position = projection * view * model * vec4(aPos, 1.0);
+	gl_Position = projection * model * vec4(aPos, 1.0);
 }
 ` + "\x00"
 
@@ -24,23 +26,38 @@ void main() {
 }` + "\x00"
 
 type Camera struct {
-	Position Vector2
-	Program  uint32
+	Position          Vector2
+	Program           uint32
+	viewUniform       int32
+	projectionUniform int32
+	projection        mgl32.Mat4
 }
 
 func CreateCamera(x, y float32) *Camera {
 	program := cameraProgram(defVertexShader, emptyFragmentShader)
 
+	/*projectionUniform = gl.GetUniformLocation(program, gl.Str("projection\x00"))
+	viewUniform = gl.GetUniformLocation(program, gl.Str("view\x00"))
+
+	projection = mgl32.Ortho2D(0, 800, 800, 0)*/
+
 	return &Camera{
-		Position: Vector2{x, y},
-		Program:  program,
+		Position:          Vector2{X: x, Y: y},
+		Program:           program,
+		projectionUniform: gl.GetUniformLocation(program, gl.Str("projection\x00")),
+		viewUniform:       gl.GetUniformLocation(program, gl.Str("view\x00")),
+		projection:        mgl32.Ortho2D(0, 800, 800, 0),
 	}
 }
 
 func (cam *Camera) UpdateCamera() {
 	gl.UseProgram(cam.Program)
 
-	//TO-DO
+	gl.UniformMatrix4fv(cam.projectionUniform, 1, false, &cam.projection[0])
+
+	view := mgl32.Translate3D(-cam.Position.X, -cam.Position.Y, 0)
+	gl.UniformMatrix4fv(cam.viewUniform, 1, false, &view[0])
+
 }
 
 func cameraProgram(vertex string, fragment string) uint32 {
