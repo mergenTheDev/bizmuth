530adb47a519d6f5937b0bb74e809610001cbc5d
Berkay <mergenthedev@proton.me>
2025-02-26 21:28:27 +0300

    Update camera.go

--- v0.0.0/camera.go
+++ v0.0.0/camera.go
@@ -1,6 +1,8 @@
 package bizmuth
 
 import (
+	"sync"
+
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/mathgl/mgl32"
 )
@@ -14,77 +16,69 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-	gl_Position = projection * model * vec4(aPos, 1.0);
+	gl_Position = projection * view * vec4(aPos, 1.0);
 }
-` + "\x00"
+`
 
 var emptyFragmentShader = `
 #version 330 core
 out vec4 FragColor;
 void main() {
 	FragColor = vec4(1.0);
-}` + "\x00"
+}`
+
+var (
+	program        uint32
+	initCameraOnce sync.Once
+	viewLoc        int32
+	projectionLoc  int32
+)
 
 type Camera struct {
-	Position          Vector2
-	Program           uint32
-	viewUniform       int32
-	projectionUniform int32
-	projection        mgl32.Mat4
+	Position   Vector2
+	projection mgl32.Mat4
 }
 
 func CreateCamera(x, y float32) *Camera {
-	program := cameraProgram(defVertexShader, emptyFragmentShader)
+	initCameraOnce.Do(func() {
+		vertexShader := compileShader(defVertexShader, gl.VERTEX_SHADER)
+		fragmentShader := compileShader(emptyFragmentShader, gl.FRAGMENT_SHADER)
 
-	/*projectionUniform = gl.GetUniformLocation(program, gl.Str("projection\x00"))
-	viewUniform = gl.GetUniformLocation(program, gl.Str("view\x00"))
+		program = gl.CreateProgram()
 
-	projection = mgl32.Ortho2D(0, 800, 800, 0)*/
+		gl.AttachShader(program, vertexShader)
+		gl.AttachShader(program, fragmentShader)
+		gl.LinkProgram(program)
+
+		projectionLoc = gl.GetUniformLocation(program, gl.Str("projection\x00"))
+		viewLoc = gl.GetUniformLocation(program, gl.Str("view\x00"))
+
+		gl.DeleteShader(vertexShader)
+		gl.DeleteShader(fragmentShader)
+	})
 
 	return &Camera{
-		Position:          Vector2{X: x, Y: y},
-		Program:           program,
-		projectionUniform: gl.GetUniformLocation(program, gl.Str("projection\x00")),
-		viewUniform:       gl.GetUniformLocation(program, gl.Str("view\x00")),
-		projection:        mgl32.Ortho2D(0, 800, 800, 0),
+		Position:   Vector2{X: x, Y: y},
+		projection: mgl32.Ortho2D(0, 800, 800, 0),
 	}
 }
 
-func (cam *Camera) UpdateCamera() {
-	gl.UseProgram(cam.Program)
+func (cam *Camera) Update() {
+	gl.UseProgram(program)
 
-	gl.UniformMatrix4fv(cam.projectionUniform, 1, false, &cam.projection[0])
+	gl.UniformMatrix4fv(projectionLoc, 1, false, &cam.projection[0])
 
 	view := mgl32.Translate3D(-cam.Position.X, -cam.Position.Y, 0)
-	gl.UniformMatrix4fv(cam.viewUniform, 1, false, &view[0])
-
+	gl.UniformMatrix4fv(viewLoc, 1, false, &view[0])
 }
 
-func cameraProgram(vertex string, fragment string) uint32 {
-	vertexShader := gl.CreateShader(gl.VERTEX_SHADER)
-	fragmentShader := gl.CreateShader(gl.FRAGMENT_SHADER)
+func compileShader(source string, shaderType uint32) uint32 {
+	shader := gl.CreateShader(shaderType)
+	shaderSource, free := gl.Strs(source + "\x00")
+	defer free()
 
-	vertexSource, freeV := gl.Strs(vertex)
-	fragmentSource, freeF := gl.Strs(fragment)
+	gl.ShaderSource(shader, 1, shaderSource, nil)
+	gl.CompileShader(shader)
 
-	gl.ShaderSource(vertexShader, 1, vertexSource, nil)
-	gl.ShaderSource(fragmentShader, 1, fragmentSource, nil)
-
-	freeV()
-	freeF()
-
-	gl.CompileShader(vertexShader)
-	gl.CompileShader(fragmentShader)
-
-	program := gl.CreateProgram()
-	gl.AttachShader(program, vertexShader)
-	gl.AttachShader(program, fragmentShader)
-	gl.LinkProgram(program)
-
-	gl.DetachShader(program, vertexShader)
-	gl.DetachShader(program, fragmentShader)
-	gl.DeleteShader(vertexShader)
-	gl.DeleteShader(fragmentShader)
-
-	return program
+	return shader
 }
