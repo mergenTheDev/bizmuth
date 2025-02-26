42081c98c70998970d6134335b9ac464c54f4c15
Berkay <mergenthedev@proton.me>
2025-02-26 22:09:07 +0300

    experimental things and added more keys to input
    Changed default OpenGL shaders (for now) and added more keys to input.

--- v0.0.0/camera.go
+++ v0.0.0/camera.go
@@ -11,20 +11,29 @@ var defVertexShader = `
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoord;
 
 uniform mat4 view;
 uniform mat4 projection;
 
+out vec2 TexCoord;
+
 void main() {
 	gl_Position = projection * view * vec4(aPos, 1.0);
+	TexCoord = aTexCoord;
 }
 `
 
 var emptyFragmentShader = `
 #version 330 core
+
 out vec4 FragColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+
 void main() {
-	FragColor = vec4(1.0);
+	FragColor = texture(ourTexture, TexCoord);
 }`
 
 var (
@@ -39,7 +48,7 @@ type Camera struct {
 	projection mgl32.Mat4
 }
 
-func CreateCamera(x, y float32) *Camera {
+func CreateCamera(x, y float32) (*Camera, uint32) {
 	initCameraOnce.Do(func() {
 		vertexShader := compileShader(defVertexShader, gl.VERTEX_SHADER)
 		fragmentShader := compileShader(emptyFragmentShader, gl.FRAGMENT_SHADER)
@@ -60,7 +69,7 @@ func CreateCamera(x, y float32) *Camera {
 	return &Camera{
 		Position:   Vector2{X: x, Y: y},
 		projection: mgl32.Ortho2D(0, 800, 800, 0),
-	}
+	}, program
 }
 
 func (cam *Camera) Update() {
@@ -68,7 +77,7 @@ func (cam *Camera) Update() {
 
 	gl.UniformMatrix4fv(projectionLoc, 1, false, &cam.projection[0])
 
-	view := mgl32.Translate3D(-cam.Position.X, -cam.Position.Y, 0)
+	view := mgl32.Translate3D(-cam.Position.X, cam.Position.Y, 0)
 	gl.UniformMatrix4fv(viewLoc, 1, false, &view[0])
 }
 
--- v0.0.0/input.go
+++ v0.0.0/input.go
@@ -17,11 +17,42 @@ const (
 	KeyA     = 65
 	KeyB     = 66
 	KeyC     = 67
+	KeyD     = 68
+	KeyE     = 69
+	KeyF     = 70
+	KeyG     = 71
+	KeyH     = 72
+	KeyI     = 73
+	KeyJ     = 74
+	KeyK     = 75
+	KeyL     = 76
+	KeyM     = 77
+	KeyN     = 78
+	KeyO     = 79
+	KeyP     = 80
+	KeyQ     = 81
+	KeyR     = 82
+	KeyS     = 83
+	KeyT     = 84
+	KeyU     = 85
+	KeyV     = 86
+	KeyW     = 87
+	KeyX     = 88
+	KeyY     = 89
+	KeyZ     = 90
 	KeySuper = 91
 )
 
+//Should call inside of bizmuth.GameLoop()
+
 func (window *Window) OnKeyPress(key glfw.Key, callback func()) {
 	if window.GetKey(key) == glfw.Press {
+		callback()
+	}
+}
+
+func (window *Window) OnKeyRelease(key glfw.Key, callback func()) {
+	if window.GetKey(key) == glfw.Release {
 		callback()
 	}
 }
