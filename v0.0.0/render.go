5e2ec92bfa232c7dbc82849c20554eff98c97fa4
Berkay <mergenthedev@proton.me>
2025-02-28 20:15:44 +0300

    Update render.go

--- v0.0.0/render.go
+++ v0.0.0/render.go
@@ -2,19 +2,25 @@ package bizmuth
 
 import "github.com/go-gl/gl/v3.3-core/gl"
 
-type Args struct {
-	pos     Vector2
-	texture uint32
-	scale   float32
+type DrawArgs struct {
+	Pos            Vector2
+	Texture        uint32
+	Scale          float32
+	Text           string
+	Font           int32
+	Physics        int32
+	Body           int32
+	CollisionShape int32
 }
 
 func BackgroundColor(r float32, g float32, b float32, alpha float32) {
 	gl.ClearColor(r, g, b, alpha)
 }
 
-func Draw(args Args) {
+func Draw(args DrawArgs) DrawArgs {
 	//TO-DO
 	/*vertices := []float32{}
 
 	indices := []float32{}*/
+	return DrawArgs{}
 }
