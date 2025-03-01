906afe84f6af228b920ea62588481c4468fb9201
Berkay <mergenthedev@proton.me>
2025-03-01 15:04:18 +0300

    Update debug.go

--- src/debug.go
+++ src/debug.go
@@ -6,8 +6,8 @@ import (
 	"github.com/go-gl/gl/v3.3-core/gl"
 )
 
-func CheckGLError() {
+func CheckRenderError() {
 	if err := gl.GetError(); err != gl.NO_ERROR {
-		log.Fatalf("OpenGL error: %v", err)
+		log.Fatalf(PrefixErr+"OpenGL Error: %v", err)
 	}
 }
