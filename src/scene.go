55cd62941ce5c0db618497d05d144e3cae67b6e6
Berkay <mergenthedev@proton.me>
2025-03-02 13:53:58 +0300

    Update scene.go

--- src/scene.go
+++ src/scene.go
@@ -2,17 +2,30 @@ package bizmuth
 
 type SceneInterface interface {
 	Ready()
+	Update()
 }
 
 type Scene struct{}
 
-func (def Scene) Ready(callback func()) {
+var CurrentScene Scene
+
+func (scene Scene) Ready(callback func()) {
 	callback()
+
 }
 
-
+func (scene Scene) Update(callback func()) {
+	go func() {
+		for {
+			callback()
+			if scene != CurrentScene {
+				break
+			}
+		}
+	}()
+}
 
 // Swithes and Runs Scene
 func SwithcScene(scene Scene) {
-	
+	CurrentScene = scene
 }
