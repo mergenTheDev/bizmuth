257c3191f1f6ac7c6fb9e5fa1dacccfdff6d2245
Berkay <mergenthedev@proton.me>
2025-03-09 12:02:38 +0300

    fix memory leak

--- src/draw.go
+++ src/draw.go
@@ -1,9 +1,14 @@
 package bizmuth
 
 import (
+	"sync"
+
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/mathgl/mgl32"
 )
+
+var genBuffersOnce sync.Once
+var vao, vbo, ebo uint32
 
 type DrawArgs struct {
 	Position       Vector2
@@ -26,37 +31,37 @@ func Draw(args DrawArgs) {
 	modelLoc := gl.GetUniformLocation(shaderProgram, gl.Str("model\x00"))
 	model := mgl32.Translate3D(args.Position.X, args.Position.Y, 0).Mul4(mgl32.Scale3D(args.Scale, args.Scale, 1))
 
-	vertices := []float32{
-		50, 50, 0, 1, 1,
-		50, -50, 0, 1, 0,
-		-50, -50, 0, 0, 0,
-		-50, 50, 0, 0, 1,
-	}
+	genBuffersOnce.Do(func() {
+		vertices := []float32{
+			50, 50, 0, 1, 1,
+			50, -50, 0, 1, 0,
+			-50, -50, 0, 0, 0,
+			-50, 50, 0, 0, 1,
+		}
 
-	indices := []uint32{
-		0, 1, 3,
-		1, 2, 3,
-	}
+		indices := []uint32{
+			0, 1, 3,
+			1, 2, 3,
+		}
 
-	var vbo, vao, ebo uint32
+		gl.GenVertexArrays(1, &vao)
+		gl.GenBuffers(1, &vbo)
+		gl.GenBuffers(1, &ebo)
 
-	gl.GenVertexArrays(1, &vao)
-	gl.GenBuffers(1, &vbo)
-	gl.GenBuffers(1, &ebo)
+		gl.BindVertexArray(vao)
 
-	gl.BindVertexArray(vao)
+		gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+		gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
 
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+		gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
+		gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indices)*4, gl.Ptr(indices), gl.STATIC_DRAW)
 
-	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
-	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indices)*4, gl.Ptr(indices), gl.STATIC_DRAW)
+		gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
+		gl.EnableVertexAttribArray(0)
 
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
-	gl.EnableVertexAttribArray(0)
-
-	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(3*4))
-	gl.EnableVertexAttribArray(1)
+		gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(3*4))
+		gl.EnableVertexAttribArray(1)
+	})
 
 	gl.UniformMatrix4fv(modelLoc, 1, false, &model[0])
 
